feb69dd44831483760ea0a3ab509a30ae533b87a
Betina <profil4e@gmail.com>
2024-11-24 18:23:27 +0200

    Adding the structure for Home Page, Login Page, Register Page + routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponentComponent},
+    {path: 'register', component: RegisterFormComponent},
+    {path: 'login', component: LoginFormComponent},
+];
